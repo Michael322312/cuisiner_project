838ce05b90e14cf150965a94cf7cdc7add81763a
Michael322312 <zomisha0808@gmail.com>
2024-06-26 16:50:18 +0300

    Added forms for models

---
+++ recipe/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from recipe.models import Category, Product, Recipe, RecipeIngridient
+
+
+class RecipeIngredientForm(forms.ModelForm):
+    class Meta:
+        model = RecipeIngridient
+        fields = ["product", "weight", "weight_unit"]
+
+
+class RecipeCreateForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+
+        fields = ["title", "main_image", "url_yt_video", "introduction", "recipe_text"]
+
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "main_image": forms.FileInput(attrs={"class": "form-control"}),
+            "url_yt_video": forms.TextInput(
+                attrs={"class": "form-control", "type": "url"}
+            ),
+            "introduction": forms.TextInput(attrs={"class": "form-control"}),
+            "ingredients": forms.Textarea(attrs={"class": "form-control"}),
+            "recipe_text": forms.TextInput(attrs={"class": "form-control"}),
+        }
+
+
+class CategoryCreateForm(forms.ModelForm):
+    class Meta:
+        model = Category
+
+        fields = ["name"]
+
+        widgets = {"name": forms.TextInput(attrs={"class": "form-control"})}
+
+        labels = {
+            "name": "Category name",
+        }
+
+
+class ProductCreateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+
+        fields = ["name", "calories", "category"]
+
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "category": forms.Select(attrs={"class": "form-select"}),
+        }
+
+        labels = {"name": "Name", "calories": "Calories (per 100 grams)"}
