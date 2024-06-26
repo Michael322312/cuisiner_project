63458d0deb664f93d7b97c6194070c10dca7f652
Michael322312 <zomisha0808@gmail.com>
2024-06-26 16:47:31 +0300

    Added product

--- recipe/models.py
+++ recipe/models.py
@@ -12,3 +12,22 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "categories"
         ordering = ["-id"]
+
+class Product(models.Model):
+    name = models.CharField(max_length=63)
+    calories = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(999999)]
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="products",
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-id"]
