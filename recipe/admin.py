ce9cc5f3ff2dca0305ed8198abcfcf556bf0265c
Michael322312 <zomisha0808@gmail.com>
2024-06-26 20:00:47 +0300

    Added to admin models

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from recipe.models import Category, Product, RecipeIngridient, Recipe
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(RecipeIngridient)
+admin.site.register(Recipe)
