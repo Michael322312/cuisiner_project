1ed06b6860ad8f53c3f02fe0a95e242994e1aa71
Michael322312 <zomisha0808@gmail.com>
2024-06-26 16:48:15 +0300

    Added recipe ingridient

--- recipe/models.py
+++ recipe/models.py
@@ -3,6 +3,8 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 
 
+UNIT_CHOISES = [("ML", "ml"), ("L", "l"), ("G", "g"), ("KG", "kg")]
+
 class Category(models.Model):
     name = models.CharField(max_length=63)
 
@@ -12,6 +14,7 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "categories"
         ordering = ["-id"]
+
 
 class Product(models.Model):
     name = models.CharField(max_length=63)
@@ -31,3 +34,17 @@ class Product(models.Model):
 
     class Meta:
         ordering = ["-id"]
+
+
+class RecipeIngridient(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    weight = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(999999)]
+    )
+    weight_unit = models.CharField(max_length=23, choices=UNIT_CHOISES, default="G")
+    recipe = models.ForeignKey(
+        "Recipe", on_delete=models.CASCADE, related_name="ingredients"
+    )
+
+    def __str__(self):
+        return f"{self.product} {self.weight} {self.weight_unit}"
