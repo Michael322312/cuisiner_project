6c35f34ca0ca9062ea20ec4b6932fc4656ede5a0
Michael322312 <zomisha0808@gmail.com>
2024-06-26 20:00:54 +0300

    Made some changes

--- recipe/urls.py
+++ recipe/urls.py
@@ -15,13 +15,9 @@ from django.urls import reverse_lazy
 
 urlpatterns = [
     path("category_list/", CategoryListView.as_view(), name="category_list"),
-    path(
-        "delete_category/<int:pk>", CategoryDeleteView.as_view(), name="delete_category"
-    ),
+    path("delete_category/<int:pk>", CategoryDeleteView.as_view(), name="delete_category"),
     path("create_category/", CategoryCreateView.as_view(), name="create_category"),
-    path(
-        "update_category/<int:pk>", CategoryUpdateView.as_view(), name="update_category"
-    ),
+    path("update_category/<int:pk>", CategoryUpdateView.as_view(), name="update_category"),
     path("product_list/", ProductListView.as_view(), name="product_list"),
     path("create_product/", ProductCreateView.as_view(), name="create_product"),
     path("delete_product/<int:pk>", ProductDeleteView.as_view(), name="delete_product"),
