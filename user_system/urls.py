0e6e0fdd685ece13ba61766cb5b6095d88a3ff09
Michael322312 <zomisha0808@gmail.com>
2024-07-15 17:07:39 +0300

    Added change password for user

--- user_system/urls.py
+++ user_system/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from user_system.views import UserCreateView, CustomLoginView, CustomLogoutView, UserSettingsView, UserUpdateView
+from user_system.views import UserCreateView, CustomLoginView, CustomLogoutView, UserSettingsView, UserUpdateView, ChangePasswordView
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="register"),
@@ -7,6 +7,7 @@ urlpatterns = [
     path("log_out/", CustomLogoutView.as_view(), name="log_out"),
     path("settings/", UserSettingsView.as_view(), name='settings'),
     path("edit_user/<int:pk>", UserUpdateView.as_view(), name="update_user"),
+    path("change_password/<int:pk>", ChangePasswordView.as_view(), name="change_password"),
 ]
 
 app_name = "user_system"
--- user_system/views.py
+++ user_system/views.py
@@ -5,8 +5,10 @@ from user_system.forms import CustomUserCreationForm, CustomUserUpdateForm
 from django.views.generic import CreateView, DeleteView, UpdateView, ListView, DetailView, TemplateView
 from user_system.models import CustomUser
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from user_system.mixins import RequestUserIsUserMixin
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 template_root = "user_system/"
@@ -19,7 +21,7 @@ class UserCreateView(CreateView):
     success_url = reverse_lazy("recipe:recipe_list")
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(LoginRequiredMixin, RequestUserIsUserMixin, UpdateView):
     form_class = CustomUserUpdateForm
     model = CustomUser
     template_name = template_root + "auth/edit_view.html"
@@ -29,7 +31,13 @@ class UserUpdateView(UpdateView):
         context = super(UserUpdateView, self).get_context_data(**kwargs)
         context['form'] = CustomUserUpdateForm(instance=self.request.user)
         return context
-    
+
+
+class ChangePasswordView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy("user_system:settings")
+    template_name = template_root + 'auth/change_password.html'
+
     
 class CustomLoginView(LoginView):
     template_name = template_root+"auth/log_in.html"
