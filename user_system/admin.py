50a574eeb3d0c42d4e964df13ec2bd34836b34f4
Michael322312 <zomisha0808@gmail.com>
2024-07-16 13:57:50 +0300

    Added to admin preference

--- user_system/admin.py
+++ user_system/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from user_system.models import CustomUser
+from user_system.models import CustomUser, UserPreference
 # Register your models here.
 
 admin.site.register(CustomUser)
+admin.site.register(UserPreference)
--- user_system/models.py
+++ user_system/models.py
@@ -9,7 +9,7 @@ class CustomUser(AbstractUser):
 
 
 class UserPreference(models.Model):
-    user = models.ForeignKey(core.settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    user = models.ForeignKey(core.settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name="preference", unique=True)
 
     fav_categories = models.ManyToManyField(Category, blank=True, related_name="fav_categories")
     hate_categories = models.ManyToManyField(Category, blank=True, related_name="hate_categories")
