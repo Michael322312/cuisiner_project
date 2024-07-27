8f5d8c267bf4feb47829bf1bcd6d76a95118486a
Michael322312 <zomisha0808@gmail.com>
2024-07-27 10:08:33 +0300

    Added security for passwords and email

--- core/settings.py
+++ core/settings.py
@@ -150,5 +150,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'cuisinersender@gmail.com'
-EMAIL_HOST_PASSWORD = 'fgidjoakszuskxkh'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
