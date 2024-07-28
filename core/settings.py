c547a650f8271d0e6a2af5bd3705872376c0195b
Michael322312 <zomisha0808@gmail.com>
2024-07-28 13:58:56 +0300

    changes

--- core/settings.py
+++ core/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DB_URL"))
+    "default": dj_database_url.parse('postgresql://cuisinerdb_v6za_user:66EIoiERhEYETlJfW9Vtx3Vlo7fixYu3@dpg-cqj1qmeehbks73c4djsg-a.frankfurt-postgres.render.com/cuisinerdb_v6za')
 }
 
 
@@ -123,10 +123,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    BASE_DIR / "static/img/",
-]
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
